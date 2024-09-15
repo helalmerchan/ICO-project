8c782463f159361c033a1f3c95b58864b8c250ef
“helalmerchan” <helalmerchan@gmail.com>
2024-09-15 19:42:03 +0600

    tokenicosol

--- contracts/TokenICO.sol
+++ contracts/TokenICO.sol
@@ -44,17 +44,51 @@ contract TokenICO{
         require(msg.value == multiply(_tokenAmount, tokenSalePrice), "Insufficient ETH for the token purchase");
 
         ERC20 token = ERC20(tokenAddress);
+
         require(_tokenAmount <= token.balanceOf(address(this)), "Not enough token left for sale");
 
         require(token.transfer(msg.sender, _tokenAmount * 1e18));
+
+        payable(owner).transfer(msg.value);
+
+        soldToken += _tokenAmount;
     }
 
-    function getTokenDetails(){}
+    function getTokenDetails() public view returns( string memory name, string memory symbol ,uint256 balance, uint256 supply, uint256 tokenPrice, address tokenAddr){
+        ERC20 token = ERC20(tokenAddress);
+        return(
+            token.name(),
+            token.symbol(),
+            token.balanceOf(address(this)),
+            token.totalSupply(),
+            tokenSalePrice,
+            tokenAddress
+        );
+    }
 
-    function transferToOwner(){}
+    function transferToOwner(uint256 _amount) external payable{
+        require(msg.value >= _amount, "Insufficient fund for the transfer");
 
-    function transferEther(){}
+        (bool success, ) =  owner.call{value: _amount}("");
 
-    function withdrawAllToken(){}
+        require(success, "Transfer failed");
+    }
+
+    function transferEther(address payable _receiver, uint256 _amount) external payable{
+        require(msg.value >= _amount, "Insufficient fund for the transfer");
+
+        (bool success, ) =  _receiver.call{value: _amount}("");
+
+        require(success, "Transfer failed");
+    }
+
+    function withdrawAllToken() public onlyOwner{
+        ERC20 token = ERC20(tokenAddress);
+        uint256 balance = token.balanceOf(address(this));
+
+        require(balance > 0, "No token to withdraw");
+        
+        require(token.transfer(owner, balance), "Transfer failed!");
+    }
 
 }
